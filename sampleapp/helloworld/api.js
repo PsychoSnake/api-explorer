87a53d23adb0a041e6005e301f39ea9279aa72c1
Carlos Guedes <cguedes@gmail.com>
2016-12-05 00:04:27 +0000

    Initial work in the no follow redirect option

--- sampleapp/helloworld/api.js
+++ sampleapp/helloworld/api.js
@@ -1,25 +1,29 @@
 module.exports = function (expressApp, port, basePath) {
-  expressApp.get('/sampleapp/helloworld/helloworld.json', function (req, res) {
+  expressApp.get('/sampleapp/helloworld/helloworld.json', (req, res) => {
     res.sendFile(basePath + req.path)
   })
 
-  expressApp.all('/sampleapp/helloworld/verbs', function (req, res) {
+  expressApp.all('/sampleapp/helloworld/verbs', (req, res) => {
     res.json({response: 'Hi'})
   })
 
-  expressApp.get('/sampleapp/helloworld/hello', function (req, res) {
+  expressApp.get('/sampleapp/helloworld/hello', (req, res) => {
     res.json({response: 'Hello World'})
   })
 
-  expressApp.all('/sampleapp/helloworld/hello/*', function (req, res) {
+  expressApp.all('/sampleapp/helloworld/hello/*', (req, res) => {
     res.json({response: req.path})
   })
 
-  expressApp.all('/sampleapp/helloworld/oldhello*', function (req, res) {
+  expressApp.all('/sampleapp/helloworld/oldhello*', (req, res) => {
     res.json({response: 'Deprecated operation'})
   })
 
-  expressApp.get('/sampleapp/helloworld/echo-status', function (req, res) {
+  expressApp.get('/sampleapp/helloworld/echo-status', (req, res) => {
     res.status(req.query.status).json({response: 'status: ' + req.query.status})
+  })
+
+  expressApp.get('/sampleapp/helloworld/redirect', (req, res) => {
+    res.redirect('/sampleapp/helloworld/hello')
   })
 }
