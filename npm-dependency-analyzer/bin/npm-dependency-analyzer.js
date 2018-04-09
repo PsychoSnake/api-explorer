a71520f4050941f4a83bc118a275779440044c13
PsychoSnake <tiago.m_lopes@hotmail.com>
2018-04-09 23:55:01 +0100

    Testing new plugin version that writes to a file the dependencies graph, vulnerabilities and licenses

--- npm-dependency-analyzer/bin/npm-dependency-analyzer.js
+++ npm-dependency-analyzer/bin/npm-dependency-analyzer.js
@@ -7,25 +7,10 @@
 
 /* eslint-disable */
 
-const { getDependencyGraph } =  require('../lib/index')
-const { existsSync, mkdirSync, openSync, writeFileSync, closeSync } = require('fs')
-const { getVulnerabilities } = require('../lib/utils/vulnerabilities')
+const index = require('../src/index')
+const { existsSync, mkdirSync } = require('fs')
 
-
-getVulnerabilities()
-
-/*
 if(!existsSync('./build')) 
 	mkdirSync('./build')
 
-getDependencyGraph('--production', writeFile)
-
-getDependencyGraph('--development', writeFile)
-
-
-function writeFile(fileName, data){
-	const fileDescriptor = openSync('build/'+fileName, 'w')
-	writeFileSync(fileDescriptor, data, 'utf-8')
-	closeSync(fileDescriptor)
-}
-*/
+index.checkProject()
--- npm-dependency-analyzer/src/index.js
+++ npm-dependency-analyzer/src/index.js
@@ -1,31 +1,61 @@
 'use strict'
 
-module.exports = getDependencyGraph
-
-const { exec } = require('executive')
-
-const commands = {
-	'--production': 'npm ls --json --prod', //Display only the dependency tree for packages in dependencies.
-	'--development': 'npm ls --json --dev'  //Display only the dependency tree for packages in devDependencies.
+module.exports = {
+	getDependencyGraph,
+	checkProject
 }
 
-function getDependencyGraph(command,cb){
-	const commandString = commands[command]
-	console.log(`Executing command: ${command}`)
-	if(command){
-		exec(commandString,
-			(error, stdout, stderr) => {
-				if(error){
-					console.log('Error: '+error)
-					console.log('Standard Error: '+stderr)
-				}
-				cb(command+'.txt', stdout)
-			})
+const exec = require('executive')
+const licenses = require('./utils/licenses')
+const vulnerabilities = require('./utils/vulnerabilities')
+const {openSync, writeFileSync, closeSync } = require('fs')
+
+const commands = {
+	'--production': 'npm la --json --prod', //Display only the dependency tree for packages in dependencies.
+	'--development': 'npm la --json --dev'  //Display only the dependency tree for packages in devDependencies.
+}
+
+const defaultCommand = 'npm la --json'
+
+function checkProject(command){
+	if(command == null){
+		console.log('[ GET DEPENDENCIES ]: Executing command for all dependencies')
+		getDependencyGraph(defaultCommand)
+		return
 	}
-	else {
+
+	const commandString = commands[command]
+	if(commandString == null){
 		console.log(`Invalid command received: ${command}. \n Commands available: \n\t--production \n\t--development`)
 		throw new Error('Invalid arguments')
 	}
+
+	console.log(`[ GET DEPENDENCIES ]: Executing command: ${command}`)
+	getDependencyGraph(commandString)
+	
+}
+
+function getDependencyGraph(commandString){
+	console.log(commandString)
+	exec.quiet(commandString, 
+		(error, stdout, stderr) => {
+			if(error){
+				throw new Error(stderr)
+			}
+			if(stdout === ''){
+				throw new Error('Invalid command')
+			}
+			
+			writeFile('dependencies.txt', stdout)
+			licenses()
+			vulnerabilities()
+		})
+}
+
+function writeFile(fileName, data){
+	const fileDescriptor = openSync('build/'+fileName, 'w')
+	writeFileSync(fileDescriptor, data, 'utf-8')
+	closeSync(fileDescriptor)
 }
 
 
