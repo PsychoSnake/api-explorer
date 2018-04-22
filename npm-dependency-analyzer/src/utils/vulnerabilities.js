4d5bf452488f15587800748175cdc494396674d9
PsychoSnake <tiago.m_lopes@hotmail.com>
2018-04-22 22:19:01 +0100

    Testing new version that writes to a file all dependencies and their information (license, vulnerabilities, etc)

---
+++ npm-dependency-analyzer/src/utils/vulnerabilities.js
@@ -0,0 +1,61 @@
+'use strict'
+
+module.exports = getVulnerabilities
+
+const fetch = require('isomorphic-fetch')
+const fileManager = require('./file-manager')
+const semver = require('semver')
+
+const debug = require('debug')('Vulnerabilities')
+const {Vulnerability} = require('../report_model')
+const RequestBody = require('../oss-fetch-body')
+
+const getRequest = body => {
+  return new Request('https://ossindex.net/v2.0/package', {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'POST',
+    body: JSON.stringify(body)
+  })
+}
+
+/**
+ * Gets all vulnerabilities on the current project
+ * Need to do POST and send all packages because sending a request for each dependency breaks the server for a bit
+ */
+async function getVulnerabilities (dependencies, cb) {
+  debug('Checking Vulnerabilities')
+
+  const requestBody = []
+
+  for (let prop in dependencies) {
+    const dependency = dependencies[prop]
+    const versions = [dependency.main_version, ...dependency.private_versions]
+    const minVersion = versions.sort(semver.compare)[0]
+
+    requestBody.push(new RequestBody('npm', dependency.title, minVersion))
+  }
+
+  const response = await fetch(getRequest(requestBody))
+  if (response.status !== 200) {
+    throw new Error('Vulnerabilities Request failed')
+  }
+  const body = await response.json()
+
+  for (let prop in body) {
+    const vulnerability = body[prop]
+    if (!vulnerability.vulnerabilities) { continue }
+
+    const vulnerabilities = vulnerability.vulnerabilities.map(elem => {
+      return new Vulnerability(elem.title, vulnerability.name,
+        elem.description, elem.references,
+        elem.versions)
+    })
+
+    dependencies[vulnerability.name].vulnerabilities = vulnerabilities
+  }
+
+  fileManager.writeBuildFile('dependencies-vulnerabilities.json', JSON.stringify(dependencies))
+  debug('End process')
+}
